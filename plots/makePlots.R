9b28fb23df4a39ab7735baea4e5ddad119543e68
jai-the-bear <jaithebear@gmail.com>
2018-11-01 20:58:03 +0530

    Fixed plots

--- plots/makePlots.R
+++ plots/makePlots.R
@@ -1,8 +1,10 @@
-for(n in names(raw)){
-file=paste(n,"png")
+raw=read.csv("diabetes.csv")
+only=raw[,-9]
+for(n in names(only)){
+file=trimws(paste(trimws(n),".png"))
 png(file)
 ftext=paste(n,"~Outcome")
 f=as.formula(ftext)
-boxplot(Pregnancies~Outcome,raw,xlab="Outcome",ylab=n,col=rainbow(2))
+boxplot(f,raw,xlab="Outcome",ylab=n,col=rainbow(2))
 dev.off()
 }
